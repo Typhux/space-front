63586a36e3e4691910595c231e4263adf15933ea
remyhny <posremy@gmail.com>
2019-03-28 22:44:05 -0400

    feat: add page login

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <div>
+      <input type="email">
+    </div>
+    <div>
+      <input type="password">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
